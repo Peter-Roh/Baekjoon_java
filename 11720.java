099d88f1addaf725f6e75ca484e39c9479b09cd5
Peter Roh(Roh, Min - Chul) <rmc2@naver.com>
2018-06-19 15:57:36 +0900

    2018.06.19
    Baekjoon java

---
+++ 11720.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+        
+        int i;
+        int sum = 0;
+
+        for(i = 0; i < n; i++)
+        {
+            sum += str.charAt(i) - '0';
+        }
+
+        System.out.println(sum);
+    }
+}
